7d2abd929d072d316111bd7b7d0c562d84e4d45c
Adam Kraus <krausadam21@gmail.com>
2023-03-30 13:30:21 +0200

    add Coala issue Edit

---
+++ src/test/java/com/codecool/atestingtw2/EditIssuesTest.java
@@ -0,0 +1,82 @@
+package com.codecool.atestingtw2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EditIssuesTest {
+    static WebDriver driver;
+    WebElement toucan;
+    WebElement coala;
+    WebElement jeti;
+
+    @BeforeEach
+    public void setUp() {
+
+        //driver setup
+        System.setProperty("webdriver.chrome.driver", "/Users/krausadam/Desktop/codecool/Advanced/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //page navigation
+        driver.get("https://jira-auto.codecool.metastage.net/login.jsp?os_destination=%2Fsecure%2FTests.jspa#/design?projectId=10101");
+
+        // precondition login
+        driver.findElement(By.name("os_username")).sendKeys("automation47");
+
+        driver.findElement(By.name("os_password")).sendKeys("CCAutoTest19.");
+
+        driver.findElement(By.name("login")).click();
+
+        //settign up variables
+        //toucan = driver.findElement(By.xpath("//*[text()='TOUCAN']"));
+
+
+    }
+
+
+    @Test
+    public void editIssueTOUCAN1(){
+        driver.get("https://jira-auto.codecool.metastage.net/browse/TOUCAN-1");
+        driver.findElement(By.id("edit-issue")).click();
+       // driver.findElement(By.id("description")).click();
+
+       // driver.findElement(By.id("description"));
+
+        driver.findElement(By.id("description")).sendKeys("               edit was good");
+        driver.findElement(By.id("edit-issue-submit")).click();
+    }
+
+    @Test
+    public void editIssueTOUCAN2()  throws InterruptedException  {
+        driver.get("https://jira-auto.codecool.metastage.net/browse/TOUCAN-2");
+        driver.findElement(By.id("edit-issue")).click();
+
+        assertTrue(driver.findElement(By.id("edit-issue-dialog")).isDisplayed());
+    }
+
+    @Test
+    public void editIssueTOUCAN3() {
+        driver.get("https://jira-auto.codecool.metastage.net/browse/TOUCAN-3");
+        driver.findElement(By.id("edit-issue")).click();
+        assertTrue(driver.findElement(By.id("edit-issue-dialog")).isDisplayed());
+    }
+
+    @AfterEach
+    public void closeup(){
+      driver.close();
+    }
+}
